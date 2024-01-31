fb1c6771b22aa06b5df2c10bcb9f9a462a8a3138
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2024-02-01 00:01:57 +0200

    upload: upload to Render

--- Server/config/db.js
+++ Server/config/db.js
@@ -3,9 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
-
 
 const pool = createPool({
   host: DB_HOST || "localhost",
@@ -15,6 +13,17 @@ const pool = createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  connectTimeout: 30000, 
 });
+
+
+pool.getConnection()
+  .then((connection) => {
+    console.log('Connected to MySQL!');
+    connection.release(); 
+  })
+  .catch((err) => {
+    console.error('Error connecting to MySQL:', err);
+  });
 
 export default pool;
