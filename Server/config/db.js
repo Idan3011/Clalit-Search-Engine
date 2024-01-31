6783479823eea997930dcd6ba87663e803c205f9
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2024-01-31 22:11:21 +0200

    upload: upload to Render

--- Server/config/db.js
+++ Server/config/db.js
@@ -6,8 +6,8 @@ dotenv.config();
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const pool = createPool({
-  host: DB_HOST || "localhost",
-  user: DB_USER || "root",
+  host: DB_HOST || "",
+  user: DB_USER || "",
   password: DB_PASSWORD || "",
   database: DB_DATABASE || "",
   waitForConnections: true,
--- Server/controllers/tableController.js
+++ Server/controllers/tableController.js
@@ -1,16 +1,22 @@
-import pool from '../config/db.js';
-import  STATUS_CODES  from '../constants/statusCode.js';
+import pool from "../config/db.js";
+import STATUS_CODES from "../constants/statusCode.js";
 
-// Controller to get all data
 export const getAllData = async (req, res) => {
   try {
-    const [rows, fields] = await pool.query('SELECT * FROM `clalit search`');
+    const page = req.query.page || 1;
+    const pageSize = req.query.pageSize || 50;
+    const offset = (page - 1) * pageSize;
+
+    const [rows, fields] = await pool.query(
+      "SELECT * FROM `clalit_search` LIMIT ?, ?",
+      [offset, Number(pageSize)]
+    );
+
     res.status(STATUS_CODES.OK).send(rows);
   } catch (error) {
-    res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).send({message: 'Internal Server Error'});
+    console.error(error);
+    res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: "Internal Server Error" });
   }
 };
-
-
-
-
