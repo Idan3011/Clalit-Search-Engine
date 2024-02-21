89f8b66dbb1d3699224b320cde691741e46327f1
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2024-02-21 22:46:10 +0200

    change: change URL to Reneder.com

--- Front/src/services/api.js
+++ Front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const URL = import.meta.env.VITE_REACT_APP_RENDER_SERVER;
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: URL,
 });
 
 export default instance;
