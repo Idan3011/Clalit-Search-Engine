14fb448da59d3357974b5ae2171e8a47224cee2e
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2024-02-22 01:21:41 +0200

    upload: upload Server & React to Render

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const PORT = import.meta.env.VITE_REACT_APP_PORT;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    port: PORT
+  }
 })
